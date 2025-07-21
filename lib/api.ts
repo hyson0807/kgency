636217459532ea7aa90b37af6ce9c445d7a88759
hyson0807 <simsgood0807@gmail.com>
2025-07-21 18:22:08 +0900

    .

--- lib/api.ts
+++ lib/api.ts
@@ -3,8 +3,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // 서버 주소 설정
 const SERVER_URL = __DEV__
-    ? 'http://192.168.0.15:5004'
+    ? 'http://192.168.0.20:5004'
     : 'https://kgencyserver-production.up.railway.app';
+
+// http://192.168.0.15:5004
 
 // API 응답 타입 (서버 응답에 맞춰 수정)
 interface ApiResponse<T = any> {
